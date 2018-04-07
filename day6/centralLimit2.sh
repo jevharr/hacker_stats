30ba0f1f41acdd6cdf2d06f3f0ad24bc519c08f3
jevharr@gmail.com <jevharr@gmail.com>
2018-04-07 13:43:38 -0700

    added day6 files

--- day6/centralLimit2.sh
+++ day6/centralLimit2.sh
@@ -4,18 +4,19 @@ input <- file('stdin', 'r')
 row1 <- readLines(input, n=1)
 row2 <- readLines(input, n=1)
 row3 <- readLines(input, n=1)
-# row1 <- "9800"
-# row2 <- "49"
-# row3 <- "205"
-# row4 <- "15"
+row4 <- readLines(input, n=1)
+# row1 <- "250"
+# row2 <- "100"
+# row3 <- "2.4"
+# row4 <- "2.0"
 row1 <- as.numeric(row1)
 row2 <- as.numeric(row2)
 row3 <- as.numeric(row3)
 row4 <- as.numeric(row4)
-wt <- row1[1]
+tix <- row1[1]
 n <- row2
 mu <- row3
 sigma <- row4
 
-p <- pnorm(wt, mean = n*mu, sd = sqrt(n)*sigma, lower.tail = T)
+p <- pnorm(tix, mean = n*mu, sd = sqrt(n)*sigma, lower.tail = T)
 write(sprintf("%.4f", p), "")
